c1aab8a3ea64e7a3ce0bc157d8d818d77437bd4b
raphael <rleitao@fluksaqua.com>
2018-06-21 16:08:41 +0200

    wip ok

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-from mocks import Post
+import os
+from flask import render_template, jsonify
+from eve import Eve
 
-app = Flask(__name__)
+app = Eve(__name__)
 
-
-#creation de fonction handler manuel
+# creation de fonction handler manuel
 # le nom utility_processor peut etre n'importe quoi
 @app.context_processor
 def utility_processor():
@@ -22,9 +22,24 @@ def utility_processor():
         return "{} {}".format(count, string)
     return dict(pluralize=pluralize)
 
-@app.route('/')
-def home():
+
+@app.route('/homepage')
+def index():
     return render_template('pages/home.html')
+
+@app.route('/<string:category>/<int:id>')
+def getAtom(category,id):
+    module_ = importlib.import_module('.'+category, package='entity')
+    class_ = getattr(module_, category.title())
+    class_.get(id)
+
+
+@app.route('/add/<string:collection>/<string:name>')
+def add(collection,name):
+    module_ = importlib.import_module('storage.mongodb')
+    class_ = getattr(module_, 'MongoStorage')
+    class_.insertPost(mongo,collection,{'name': name})
+    return 'added user name {} !'.format(name)
 
 @app.route('/about')
 def about():
@@ -36,16 +51,14 @@ def posts_index():
     return render_template('posts/index.html', posts=Post.all())
 
 
-
-@app.route('/blog/posts/<int:id>')
+@app.route('/blog/get/<int:id>')
 def posts_show(id):
     post = Post.find(id)
     return render_template('posts/show.html', post=post)
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('errors/404.html'), 404
-
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host=host, port=port)
